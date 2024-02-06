2a5e8e8ac13533d86bc1833b14b007fcb13a2f9e
imtiaz-cnits <ia64744@gmail.com>
2024-02-06 22:40:22 +0600

    live test 02 done

---
+++ index.js
@@ -0,0 +1,10 @@
+// Question : Write a function named createServer that creates a simple HTTP server using the built-in http module.
+// The server should listen on port 5000 and respond to all requests with a plain text response of "Hello This is Node Js Server!".
+
+const http = require("http");
+
+const createServer = http.createServer((req, res) => {
+  res.end("Hello This is Node Js Server!");
+});
+
+createServer.listen(5000);
