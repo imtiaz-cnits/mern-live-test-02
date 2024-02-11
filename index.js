dafc8188e443b20a1a2cc62b16deec8851887c4e
imtiaz-cnits <ia64744@gmail.com>
2024-02-11 22:42:38 +0330

    another simple push

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // Question : Write a function named createServer that creates a simple HTTP server using the built-in http module.
 // The server should listen on port 5000 and respond to all requests with a plain text response of "Hello This is Node Js Server!".
+// Simple server config
 
 const http = require("http");
 
